282c932d0834286b317d4958370b2c185be477c7
nishaya raj <nishayaraj@gmail.com>
2022-05-22 22:15:13 -0500

    Added on click button to render lyrics on dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,48 @@ import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
+const renderToDom = (divId, htmlToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToPrint;
+};
+
+// HTML Structure
+const htmlStructure = () => {
+  const domString = `<div id="btn"></div>
+  <div id="lyrics-container"></div>`;
+  renderToDom('#app', domString);
+};
+
+// API Call : get the call & returns data.
 const getLyrics = (artist, song) => new Promise((resolve, reject) => {
   axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
 });
 
+// HTML on the dom.
 const lyricsOnDom = (artist, song) => {
+// this is a promise.
   getLyrics(artist, song).then((response) => {
-    document.querySelector('#app').innerHTML = response.lyrics;
+    renderToDom('#lyrics-container', response.lyrics);
+  });
+};
+
+const button = () => {
+  const domString = '<button id="song-btn" class = "btn btn-primary">Click Me</button>';
+  renderToDom('#btn', domString);
+};
+
+const events = () => {
+  document.querySelector('#song-btn').addEventListener('click', () => {
+    lyricsOnDom('prince', 'purple rain');
   });
 };
 
 const startApp = () => {
-  lyricsOnDom('prince', 'purple rain');
+  htmlStructure();
+  button();
+  events();
 };
 
 startApp();
